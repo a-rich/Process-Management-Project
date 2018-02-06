b4fb6c68ff190b25d0a7537004f103ba247e9334
Nathan <nateinthegame@gmail.com>
2018-02-06 10:29:17 -0800

    Implemented SASS

--- project/client/like-home/src/App.js
+++ project/client/like-home/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 import {Nav, Navbar, NavDropdown, MenuItem, Tabs, ButtonToolbar, Button, Table, ButtonGroup, Row, Col, Grid, Panel, FormGroup, FormControl} from 'react-bootstrap';
 
@@ -8,22 +8,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar inverse >
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#home">Brand</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Navbar.Form pullLeft>
-            <FormGroup>
-              <FormControl type="text" placeholder="Search" />
-            </FormGroup>{' '}
-            <Button type="submit">Submit</Button>
-          </Navbar.Form>
-        </Navbar.Collapse>
-      </Navbar>;
+        <Navbar inverse >
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Brand</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+              </FormGroup>{' '}
+              <Button type="submit">Submit</Button>
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>;
+      
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
