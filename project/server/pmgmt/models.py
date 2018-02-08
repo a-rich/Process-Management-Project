90bf8a8567c6ab7fc737e02e74b4aeaae7864261
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-02-08 01:52:40 -0800

    added initial model for user, storing hashed password

--- project/server/pmgmt/models.py
+++ project/server/pmgmt/models.py
@@ -1,12 +1,20 @@
-# from flask_mongoengine import MongoEngine
+#project/server/pmgmt/models.py
 
-# db = MongoEngine()
+import datetime
+from project.server import app, db, bcrypt
 
-# class User(db.Document):
-#    email = db.EmailField()
-#    password = db.StringField(default='')
-#    name = db.StringField(default='')
-#
-#    @staticmethod
-#    def exists(email):
-#        return User.objects(email=email)
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    registed_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    
+
+    def __init__(self, name, email, password, admin=False):
+        self.email = email
+        self.password_hash = bcrypt.generate_password_hash(password)
+        self.name = name
