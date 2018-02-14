e1cdf2acf31d6a8a2ea1eae561d027970e9f5da4
samnang sok <samnangsok.se@gmail.com>
2018-02-14 01:24:05 -0800

    sign up modal ui

--- project/client/like-home/src/components/modals/SignUp.js
+++ project/client/like-home/src/components/modals/SignUp.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import '../../stylesheets/SignUpForm.scss'
 
 const customStyles = {
     content : {
       top                   : '50%',
       left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
+      right                 : '70%',
+      bottom                : 'auto%',
       marginRight           : '-50%',
       transform             : 'translate(-50%, -50%)'
+
     }
   };
 
@@ -39,7 +41,11 @@ class SignUp extends Component {
     render() {
         return (
             <div>
+
             <a onClick={this.openModal}>Sign Up</a>
+
+            <form>
+
             <Modal
               isOpen={this.state.modalIsOpen}
               onAfterOpen={this.afterOpenModal}
@@ -47,10 +53,10 @@ class SignUp extends Component {
               style={customStyles}
               contentLabel="Sign Up"
             >
-    
-                <h2 ref={subtitle => this.subtitle = subtitle}>CREATE AN ACCOUNT</h2>
            
-              <form>
+     
+            <h2 ref={subtitle => this.subtitle = subtitle}>CREATE AN ACCOUNT</h2>
+
             <p>Please fill in this form to create an account.</p>
             <hr/>
              <label for="email"><b>Email Address</b></label>
@@ -69,8 +75,11 @@ class SignUp extends Component {
              <p>By creating an account you agree to our <a href="#" >Terms & Privacy</a>.</p>
             <button type="submit" class="button2">Sign Up</button>
                <p>Already have an account? <a href="#" >Sign in</a>.</p>
-              </form>
-            </Modal>
+
+           </Modal>
+         </form>  
+
+
           </div>
         );
     }
