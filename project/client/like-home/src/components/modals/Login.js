79df11e82cc5ea67422906e1610636e63b1f44d6
samnang sok <samnangsok.se@gmail.com>
2018-02-14 23:23:07 -0800

    login screen update

--- project/client/like-home/src/components/modals/Login.js
+++ project/client/like-home/src/components/modals/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
 import '../../stylesheets/SignUp.scss'
+import {Row, Col, Grid} from 'react-bootstrap';
 
 const customStyles = {
     content : {
@@ -41,39 +42,45 @@ class Login extends Component {
       }
 
     render() {
-        return (
+               return (
             <div>
 
             <a onClick={this.openModal}>Login</a>
-
+           
             <form>
-
-            <Modal
+              <Modal
               isOpen={this.state.modalIsOpen}
               onAfterOpen={this.afterOpenModal}
               onRequestClose={this.closeModal}
               style={customStyles}
               contentLabel="Sign Up"
-            >
-           
-            <h2 ref={subtitle => this.subtitle = subtitle}>Sign In</h2>
-            <hr/>
-             <label for="email"><b>Email Address</b></label>
-            <input class="make-it-slow" type="word" placeholder="Enter Email" name="email" required aria-required="true"/>              
-            <label for="psw"><b>Password</b></label>
-            <input class="make-it-slow" type="password" placeholder="Enter Password" name="psw" required/>
-                
-             <p><a href="#" >Forgot your password?</a></p>
-            <button type="submit" class="button2">LogIn</button>
-               <p>Don't have an account? <a href="#" >Sign Up</a>.</p>
 
+              >
+              <div className="row">
+                <div className="col-4">
+                  <h2 className="text-center" ref={subtitle => this.subtitle = subtitle}>Sign In</h2>
+                  <hr/>
+                </div>
+                <div className="col-4">
+                  <label for="email"><b>Email Address</b></label>
+                  <input class="make-it-slow" type="word" placeholder="Enter Email" name="email" required/> 
+                </div>
+                <div className="col-4">
+                  <label for="psw"><b>Password</b></label>
+                  <input class="make-it-slow" type="password" placeholder="Enter Password" name="psw" required/>
+                </div>
+                <div className="col-4">
+                  <p><a href="#" >Forgot your password?</a></p>
+                  <button type="submit" class="button2">Login</button>
+                  <p>Don't have an account? <a href="#" >Sign Up</a>.</p>
+                </div>
+              </div> 
            </Modal>
          </form>  
 
 
           </div>
-        );
-    }
+        );    }
 }
 
 export default Login;
