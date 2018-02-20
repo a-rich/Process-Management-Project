e89e8c1dca999614b32babfe2f871e62957d23cf
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-02-19 19:31:15 -0800

    styled some css to fix flex layout issues, and then set a border radius for the images and descriptions

--- project/client/like-home/src/components/CollectionView.js
+++ project/client/like-home/src/components/CollectionView.js
@@ -7,7 +7,33 @@ export class CollectionView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [1,2,3,4,5,6,7,8]
+      items: [
+        {'name':"Las Vegas",
+          'photo': "https://d1ic4altzx8ueg.cloudfront.net/finder-au/wp-uploads/2016/02/LasVegasSign.Shutterstock-250x250.jpg"
+        },
+        {'name':"Miami",
+        'photo': "http://www.djoybeat.com/wp-content/uploads/2014/07/miami3-250x250.jpg"
+        },
+        {'name':"London",
+          'photo': "https://d1ic4altzx8ueg.cloudfront.net/finder-au/wp-uploads/2017/04/best-time-to-go-to-london-feature-250x250.jpg"
+        },
+        {'name':"San Francisco",
+         'photo': "http://tomsphotocafe.com/wp-content/uploads/2016/06/WS_130630-USA-CA-San-Francisco-8TP2935-250x250.jpg"
+        },
+        {'name':"Los Angeles",
+        'photo': "http://www.djoybeat.com/wp-content/uploads/2014/05/Los_Angeles-250x250.jpg"
+        },
+        {'name':"New York",
+        'photo': "http://tomsphotocafe.com/wp-content/uploads/2016/03/WS_NewYork_8TP8173_4_5_6_7E-1-250x250.jpg"
+      },
+        {'name':"Paris",
+        'photo': "https://paris-day-trip.com/wp-content/uploads/2016/10/Paris-Arc-De-Triomphe-Gallery-250x250.jpg"
+
+      },
+        {'name':"Tokyo",
+        'photo': "https://d1ic4altzx8ueg.cloudfront.net/finder-au/wp-uploads/2017/12/Tokyo-things-to-do-Feature-250x250.jpg"
+      },
+      ]
     }
   }
 
@@ -36,14 +62,15 @@ export class CollectionView extends React.Component {
         <div className="items">
           {this.state.items.map((item, index) => {
             return (
-              <Link to={`/item/${this.state.items[index].id}`} key={index} className="itemLink">
-                <img src={'http://via.placeholder.com/400x250'}/>
-
-                <div className="itemInfo">
-                  <h3>Destination Name</h3>
-                  <p>Destination Location</p>
+              <div className="itemLink">
+              <Link to={`/item/${this.state.items[index].name}`} key={index} className="">
+                <img src={item.photo}/>
+                <div className="info">
+                  <h3>{item.name}</h3>
                 </div>
               </Link>
+            </div>
+
             )
           })}
         </div>
