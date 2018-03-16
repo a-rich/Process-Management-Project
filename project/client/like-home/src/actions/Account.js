70283d3a9bd1e01b7b4fe1e5a21d29b0f9e0ce53
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-03-15 22:46:12 -0700

    cleaned up create user function and created a login function that stores jwt token and user name in local storage

--- project/client/like-home/src/actions/Account.js
+++ project/client/like-home/src/actions/Account.js
@@ -5,15 +5,25 @@ const ROOT_URL = "http://127.0.0.1:5000"
 export function createUser(values) {
     const request = axios.post(`${ROOT_URL}/api/create_user/`, values)
     .then(function (response) {
-        console.log("response!!!!")
         console.log(response);
     })
     .catch(function (error) {
     console.log(error);
-  });;
-    console.log("CREATE USER");
-    return {
-        //type: create_user,
-       // payload: request
-    }
+  });
+    return { }
+}
+
+export function login(values){
+  const request = axios.post(`${ROOT_URL}/api/login/`, values)
+  .then(function(response){
+    console.log(response);
+    console.log(response.data.jwt)
+    localStorage.jwt = response.data.jwt
+    localStorage.currenUserName = response.data.name
+  })
+  .catch(function (error){
+    console.log(error)
+  })
+
+  return {}
 }
