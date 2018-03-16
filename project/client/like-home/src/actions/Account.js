c54cdf515bbd286bdc22970e868a37cc18d7b82c
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-03-15 19:07:19 -0700

    succesully connect to backend with preset data

--- project/client/like-home/src/actions/Account.js
+++ project/client/like-home/src/actions/Account.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
-const ROOT_URL = "http://localhost:3000/"
+const ROOT_URL = "http://127.0.0.1:5000"
 
 
 export function createUser(values) {
-    const request = axios.post('${ROOT_URL}/api/create_user/', values);
+    const request = axios.post(`${ROOT_URL}/api/create_user/`, values)
+    .then(function (response) {
+        console.log("response!!!!")
+        console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+  });;
     console.log("CREATE USER");
     return {
         //type: create_user,
