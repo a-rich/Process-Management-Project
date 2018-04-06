44c527de8f38c2104dbc329ebd7454a352701146
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-04-06 11:43:43 -0700

    parallaxcomp removed and readded collection view to explore

--- project/client/like-home/src/views/Explore/Explore.js
+++ project/client/like-home/src/views/Explore/Explore.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Mainbar from '../../components/MainBar'
-import ParallaxComp from '../../components/ParallaxComp'
 import CollectionView from '../../components/CollectionView'
 const Explore = () => (
   <div>
     <Mainbar/>
-    
-    <ParallaxComp/>
+    <CollectionView/>
   </div>
 );
 
