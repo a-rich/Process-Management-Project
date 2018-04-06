1a316a3bfa0780e8cfbc6a1dc015afb95f61f185
elihabash23 <eliashabash23@yahoo.com>
2018-04-06 12:27:18 -0700

    account page

--- project/client/like-home/src/views/Account/Account.js
+++ project/client/like-home/src/views/Account/Account.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './Account.scss'
 import scrollToComponent from 'react-scroll-to-component'
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Grid, Row, Col, Button} from 'react-bootstrap'
 
 class Account extends Component {
     constructor(props) {
@@ -24,8 +24,10 @@ class Account extends Component {
                     <Col sm={2} md={2} lg={2}>
                         
                         <div className="sideBar">
-                            <button onClick={() => scrollToComponent(this.MyBookings, { offset: 0, align: 'top', duration: 1500})}>My Bookings</button>
-                            <button onClick={() => scrollToComponent(this.MyRewards, { offset: 0, align: 'top', duration: 1500})}>Rewards</button>
+                            <Button block bsClass="accountButton" onClick={() => scrollToComponent(this.MyBookings, { offset: 0, align: 'top', duration: 1500})}>My Bookings</Button>
+                            <Button block bsClass="accountButton" onClick={() => scrollToComponent(this.MyRewards, { offset: 0, align: 'top', duration: 1500})}>Rewards</Button>
+                            <Button block bsClass="accountButton" onClick={() => scrollToComponent(this.MyFavorites, { offset: 0, align: 'top', duration: 1500 })}>Favorites</Button>
+                            <Button block bsClass="accountButton" onClick={() => scrollToComponent(this.MyUserInfo, { offset: 0, align: 'top', duration: 1500 })}>User Information</Button>
                         </div>
                     </Col>
                     <Col sm={10} md={10} lg={10}>
@@ -34,7 +36,20 @@ class Account extends Component {
                                 <h4>MyBookings</h4>
 
                             </section>
-                            <section className='MyRewards' ref={(section) => { this.MyRewards = section; }}>Rewards</section>
+                            
+                            <section className='MyRewards' ref={(section) => { this.MyRewards = section; }}>
+                                <h4>Rewards</h4>
+                            </section>
+
+                            <section className='MyFavorites' ref={(section) => { this.MyFavorites = section; }}>
+                                <h4>Favorites</h4>
+                            </section>
+
+                            <section className='MyUserInfo' ref={(section) => { this.MyUserInfo = section; }}>
+                                <h4>User Information</h4>
+                            </section>
+
+
                         </div>
                     </Col>
                 </Row>
