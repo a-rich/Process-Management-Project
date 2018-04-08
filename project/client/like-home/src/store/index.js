a0baa77171b68a5e5fe61900a439b350c461dd0c
Alexis Candelaria <alexiscandelaria1@gmail.com>
2018-04-08 00:49:58 -0700

    added a store folder with an index file and all the reducers

---
+++ project/client/like-home/src/store/index.js
@@ -0,0 +1,22 @@
+import C from '../constants'
+import appReducer from './reducers'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+
+const consoleMessages = store => next => action => {
+  let result
+  console.groupCollapsed(`dispatching action => ${action.type}`)
+  console.log('current user', store.getState().currentUser)
+  result = next(action)
+  let { currentUser } = store.getState()
+
+  console.log(`
+    current user: ${currentUser}
+    `)
+  console.groupEnd()
+  return result
+}
+
+export default (initialState={}) => {
+  return applyMiddleware(thunk, consoleMessages)(createStore)(appReducer, initialState)
+}
