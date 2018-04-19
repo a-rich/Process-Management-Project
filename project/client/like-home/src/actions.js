e50dcf581543d5228229472b04c3f941e4728713
Nathan <nateinthegame@gmail.com>
2018-04-19 14:25:30 -0700

    REDUX!!!!!!!!!!

--- project/client/like-home/src/actions.js
+++ project/client/like-home/src/actions.js
@@ -4,3 +4,8 @@ export const setcurrentuser = (name, token) => ({
       type: C.SET_CURRENTUSER,
           payload: {"currentUser":  {name, token}}
    })
+
+export const setSearchResults = (searchResults) => ({
+type: C.SET_SEARCHRESULTS,
+    payload: {"searchResults":  {searchResults}}
+})
