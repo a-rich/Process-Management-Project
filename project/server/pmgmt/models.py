f95f1b9ac0fcee29b3bb11a1b36086cfd6c011f8
Nathan <nateinthegame@gmail.com>
2018-04-19 15:02:42 -0700

    SALVATION

--- project/server/pmgmt/models.py
+++ project/server/pmgmt/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, SmallInteger, DateTime, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, SmallInteger, Date, DateTime, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from pmgmt import db, ma, whooshee
+from pmgmt import db, ma
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -11,14 +11,12 @@ class User(db.Model):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
+    reward = Column(Integer)
 
-    class UserSchema(ma.ModelSchema):
-        class Meta:
-            def __init__(self):
-                model = self.Outer.Outer
 
-@whooshee.register_model('location')
 class Hotel(db.Model):
+    __tablename__ = 'hotel'
+    __searchable__ = ['name', 'location']
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -27,19 +25,14 @@ class Hotel(db.Model):
     image_url = Column(Text)
     rating = Column(Float)
     coordinates = Column(String(250))
-    price = Column(Float)
+    price = Column(String(50))
     tiers = Column(String(250))
     location = Column(String(250))
+    city = Column(String(250))
+    state = Column(String(250))
     address = Column(Text)
     phone = Column(String(20))
 
-    class HotelSchema(ma.ModelSchema):
-        class Meta:
-            fields = ('name', 'location')
-            def __init__(self):
-                model = self.Outer.Outer
-
-    hotel_schema = HotelSchema(many=True)
 
 class Room(db.Model):
     __tablename__ = 'room'
@@ -49,14 +42,16 @@ class Room(db.Model):
     hotel_id = Column(Integer, ForeignKey('hotel.id'))
     hotel = relationship(Hotel)
     price = Column(Float)
-    tiers = Column(String(250))
-    location = Column(String(250))
-    phone = Column(String(250))
+    tier = Column(String(250))
+    vacant = Column(SmallInteger, default=0)
+    created_date = Column(DateTime(timezone=True), server_default=func.now())
+
 
 class Tier(db.Model):
     __tablename__ = 'tier'
     id = Column(Integer, primary_key=True)
     tier = Column(String(250))
+
 
 class Reservation(db.Model):
     __tablename__ = 'reservation'
@@ -69,3 +64,38 @@ class Reservation(db.Model):
     room = relationship(Room)
     active = Column(SmallInteger)
     created_date = Column(DateTime(timezone=True), server_default=func.now())
+    updated_date = Column(DateTime(timezone=True), server_default=func.now())
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+    class ReservationSchema(ma.ModelSchema):
+        class Meta:
+            def __init__(self):
+                model = self.Outer.Outer
+
+    reservations_schema = ReservationSchema(many=True)
+
+
+class Reward(db.Model):
+    __tablename__ = 'reward'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    reservation_id = Column(Integer, ForeignKey('reservation.id'))
+    reservation = relationship(Reservation)
+    alter = Column(Integer)
+    reward = Column(Integer)
+    active = Column(SmallInteger)
+    created_date = Column(DateTime(timezone=True), server_default=func.now())
+
+class State(db.Model):
+    __tablename__ = 'state'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('state.id'))
+    state = relationship(State)
+    name = Column(String(50))
---
+++ project/server/pmgmt/views/reservations.py
@@ -0,0 +1,154 @@
+import time, datetime
+from itsdangerous import URLSafeTimedSerializer
+from sqlalchemy import and_, or_
+from flask import current_app, Blueprint, request, url_for, render_template, jsonify
+from flask_jwt_simple import create_jwt, jwt_required, get_jwt_identity
+from flask_marshmallow import Marshmallow
+from pmgmt.models import db, User, Reservation, Hotel, Room, Reward
+
+
+reservation = Blueprint('reservation', __name__)
+
+requestLogInMsg = 'Sorry! You need to log in!'
+
+
+@reservation.route('/api/reservation/new/', methods=['POST'])
+# @jwt_required
+def new_reservation():
+
+#insert into user (name,email,password,reward) values ('jj','jiajun.wu@sjsu.edu','',5000);
+
+	req = request.get_json()
+	#roomId = request.form['room_ID']
+	roomId = req['room_ID']
+	startDate = req['start_date']
+	endDate = req['end_date']
+	redeem = req['redeem']
+
+	try:
+		# user = User.query.filter(User.email==get_jwt_identity()).first()
+		#user = User.query.filter(User.email=='jiajun.wu@sjsu.edu').first()
+		# created a user in sqlite to test this, if you want to test this endpoint, do the same, and provide your email here,
+		
+		user = User.query.filter(User.email=='brandonabajelo@gmail.com').first()
+
+
+		if user:
+			# try_create_res = db.session.query(Reservation).filter(Reservation.room_id==roomId).filter(Reservation.start_date.between(startDate, endDate)|Reservation.end_date.between(startDate, endDate))
+			try_create_res = Reservation.query.filter(Reservation.room_id==roomId, Reservation.active==1) \
+				.filter(or_(and_(Reservation.start_date>=startDate, Reservation.start_date<endDate), and_(Reservation.end_date>startDate, Reservation.end_date<=endDate))) \
+				.first()
+				# .filter(Reservation.start_date.between(startDate, endDate)|Reservation.end_date.between(startDate, endDate))
+				# .filter(startDate>=start_date, startDate<end_date)|(endDate>start_date, endDate<=end_date))
+			# print(try_create_res)
+
+			if not try_create_res:
+				try:
+					startDate=datetime.datetime.strptime(startDate, "%Y-%m-%d").date()
+					endDate=datetime.datetime.strptime(endDate, "%Y-%m-%d").date()
+					room = Room.query.join(Hotel).filter(Hotel.id==Room.hotel_id, Room.id==roomId).first()
+					new_reservation = Reservation(user_id=user.id, hotel_id=room.hotel_id, room_id=roomId, active=1, start_date=startDate, end_date=endDate)
+					db.session.add(new_reservation)
+					db.session.flush()
+
+					roomPrice = int(room.price)
+
+					reward = Reward(user_id=user.id, reservation_id=new_reservation.id, alter=roomPrice, reward=user.reward+roomPrice, active=1)
+					db.session.add(reward)
+
+					user.reward = user.reward+roomPrice
+
+					db.session.commit()
+
+					return jsonify({'msg': 'Reservation is been made!'})
+				except Exception as e:
+					db.session.rollback()
+					return jsonify({'msg': str(e)})
+
+			else:
+				return jsonify({'msg': 'Sorry! This room is not available!'})
+		else:
+			return jsonify({'msg': requestLogInMsg})
+
+	except Exception as e:
+		return jsonify({'msg': str(e)})
+
+
+@reservation.route('/api/reservation/update/', methods=['POST'])
+# @jwt_required
+def update_reservation():
+
+	req = request.get_json()
+	resId = req['reservation_ID']
+	startDate = req['start_date']
+	endDate = req['end_date']
+
+	try:
+		# user = User.query.filter(User.email==get_jwt_identity()).first()
+		user = User.query.filter(User.email=='jiajun.wu@sjsu.edu').first()
+
+		if user:
+			res = Reservation.query.filter(Reservation.id==resId).first()
+
+			if res:
+				"""i need to filter the starting date if conflict with end date whthin the same room"""
+				room = Room.query.filter(Room.id==res.room_id)
+				try_update_res = Reservation.query.filter(Reservation.id!=res.id, Reservation.room_id==res.room_id, Reservation.active==1) \
+					.filter(or_(and_(Reservation.start_date>=startDate, Reservation.start_date<endDate), and_(Reservation.end_date>startDate, Reservation.end_date<=endDate))) \
+					.first()
+
+				if not try_update_res:
+					startDate = datetime.datetime.strptime(startDate, "%Y-%m-%d").date()
+					endDate = datetime.datetime.strptime(endDate, "%Y-%m-%d").date()
+					res.updated_date = datetime.datetime.now()
+					res.start_date = startDate
+					res.end_date = endDate
+					db.session.commit()
+				else:
+					return jsonify({'msg': 'Can not reserve the room due to time conflict!'})
+
+			else:
+				return jsonify({'msg': 'Sorry! Can not find this reservation!'})
+
+			return jsonify({'msg': 'Reservation is been updated!'})
+		else:
+			return jsonify({'msg': requestLogInMsg})
+
+	except Exception as e:
+		return jsonify({'msg': str(e)})
+
+
+@reservation.route('/api/reservation/cancel/', methods=['POST'])
+# @jwt_required
+def cancel_reservation():
+
+	req = request.get_json()
+	resId = req['reservation_ID']
+
+	try:
+		# user = User.query.filter(User.email==get_jwt_identity()).first()
+		user = User.query.filter(User.email=='jiajun.wu@sjsu.edu').first()
+
+		if user:
+			res = Reservation.query.filter(Reservation.user_id==user.id, Reservation.id==resId, Reservation.active==1).first()
+
+			if res:
+				res.active = -1
+				res.updated_date = datetime.datetime.now()
+
+				user_reward = Reward.query.filter(Reward.user_id==user.id, Reward.reservation_id==resId, Reward.active==1).first()
+				user_reward.active = -1
+				user_reward.reward -= user_reward.alter
+
+				user.reward -= user_reward.alter
+
+				db.session.commit()
+			else:
+				return jsonify({'msg': 'Sorry! Can not find this reservation!'})
+
+			return jsonify({'msg': 'Reservation is been canceled!'})
+		else:
+			return jsonify({'msg': requestLogInMsg})
+
+	except Exception as e:
+		return jsonify({'msg': str(e)})
