8d76e116bfc9759f92748d59febf73a7b61e5b04
Nathan <nateinthegame@gmail.com>
2018-04-27 13:32:40 -0700

    update

--- project/client/like-home/src/components/ListView.js
+++ project/client/like-home/src/components/ListView.js
@@ -1,24 +1,60 @@
-import React from 'react'
-import { Component } from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import {ListGroup, ListGroupItem, Grid, Row, Col, Button, buttonStyle, Checkbox, Glyphicon} from 'react-bootstrap'
 import '../stylesheets/ListView.scss'
 import {searchHotels} from '../actions/Search'
+import { setSearchResults } from '../actions.js'
+
+const mapStateToProps = (searchHotels) => ({
+  searchHotels
+})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setSearchResults(results) {
+      dispatch(searchHotels)
+    }
+  }
+}
 
 class ListView extends Component {
+  static propTypes = {
+    searchHotels: PropTypes.func.isRequired
+  }
 
-  constructor(props) {
-    console.log(window.store.getState().searchResults.searchResults)
-    super(props);
-   
-    
-    this.state = {
-        items: [ [].concat.apply([], window.store.getState().searchResults.searchResults)
-        ]
-    };
+  static defaultProps = { 
+    items: new Map()
+  }
+
+  state = {
+    items: [ ]
+}
+
+componentWillMount() {
+this.setState({
+  items: [[].concat.apply([], window.store.getState().searchResults.searchResults.slice(1, 20))]})
+}
+
+componentDidMount() {
+  console.log("componentDidMount")
+}
+
+componentWillReceiveProps() {
+  console.log("componentWillReceiveProps")
+}
+
+componentDidUpdate() {
+  console.log("componentDidUpdate")
+}
+
+  constructor(props, context) {
+    super(props, context);
   }
 
   render() {
-    
+    const { items } = this.state
+    console.log("ITESMS",items)
  return(
      <Grid>
         <ListGroup>
@@ -57,4 +93,4 @@ class ListView extends Component {
  )
 }
 };
-export default ListView;
+export default connect(mapStateToProps, mapDispatchToProps) (ListView);
