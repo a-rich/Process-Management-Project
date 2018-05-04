79e82b350a503db4911924eb74081704a468c605
Marta Malapitan <marta_mm1995@yahoo.com>
2018-05-04 14:25:17 -0700

    Payment form created

--- project/client/like-home/src/components/PaymentForm.js
+++ project/client/like-home/src/components/PaymentForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardSection from '../components/CardSection';
-import {injectStripe} from 'react-stripe-elements'
+import {injectStripe} from 'react-stripe-elements';
+import {Grid, Row, Col} from 'react-bootstrap';
 // import ReactScriptLoaderMixin from 'react-script-loader'
 
 // var ReactScriptLoaderMixin = require('react-script-loader').ReactScriptLoaderMixin;
@@ -9,17 +10,48 @@ import {injectStripe} from 'react-stripe-elements'
 class PaymentForm extends Component {
     constructor(props) {
     super(props);
+        this.state = {
+            name: 'Name',
+            card: 'Card Number',
+            exp: 'Expiration',
+            cvc: 'CVC'
+        }
+        this.updateState = this.updateState.bind(this);
+      };
+      updateState(e) {
+          this.setState({name: e.target.value});
       }
 
     render() {
         return (
-      <div>
-          {/* <CardSection /> */}
-        Forms
+      <div id="paymentForm">
+        <h3 class="header">Pay Now</h3>
+        <form>
+        <Grid><Row>
+        <Col>
 
-			</div>
+        <input type = 'text' id='nameBox' value = {this.props.name}
+            onChange = {this.updateState} placeholder="Full Name" /> <br />  <br />
+
+        <input type = "text" id='cardBox' value = {this.props.card}
+        onChange = {this.updateState} />  <br />  <br />
+        </Col>
+
+        <Col>
+        <input type = "text" id='expBox' value = {this.props.exp}
+        onChange = {this.updateState} />
+
+        <input type = "text" id='cvcBox' value = {this.props.cvc}
+        onChange = {this.updateState} /> <br />  <br />
+        </Col>
+        <input type="submit" id='payButton' value="Pay" />
+        </Row></Grid>
+        </form>
+	</div>
         );
     }
 }
 
-export default injectStripe(PaymentForm);
+export default PaymentForm;
+
+// export default injectStripe(PaymentForm);
