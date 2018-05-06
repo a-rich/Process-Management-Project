c36871e1aef97f6db7cef7644399af147c87eaf4
samnang sok <samnangsok.se@gmail.com>
2018-05-06 16:17:47 -0700

    logout bug fixed. redirect users back to the homepage after being logged out

--- project/client/like-home/src/components/Logout.js
+++ project/client/like-home/src/components/Logout.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Component } from 'react'
-
+import Explore from "../views/Explore/Explore"
 
 class Logout extends Component {
   logout(e){
     e.preventDefault();
     localStorage.clear();
-    window.location.reload(true);
+    document.location.href="/";
   }
 
 render() {
  return(
-        <a href="#" onClick={this.logout}>Logout</a>
+        <a href="details.html" onClick={this.logout}>Logout</a>
        )
     }
 }
